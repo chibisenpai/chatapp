8f077dec7eff79ba61894d9a7d92d8bf4c9ca50b
chibisenpai <ayaanm0980@gmail.com>
2025-05-17 23:32:45 +0500

    4.0

--- client.c
+++ client.c
@@ -5,10 +5,10 @@
 int int client()
 {
   int socketFD = socket(domain: AF_INET, type: SOCK_STREAM, protocol: 0);
-  
+  char* ip = "64.233.180.26"
   struct sockaddr_in address;
   address.sin_farmily = AF_INET;
-  address.sin_port = 2000;
+  address.sin_port = htons(hostshort: 80);
   address.sin_addr;
   inet_pton(af: AF_INET,cp: ip, buf:&address.sin_addr.s_addr);
 
